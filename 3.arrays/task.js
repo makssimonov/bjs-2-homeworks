c516eb7439e6e2689bc554f02d8bda2166db45e8
makssimomov <makssimonov96@gmail.com>
2022-07-08 22:49:56 +0500

    homework added

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,19 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
+  
+  result = arr1.length === arr2.length && arr1.every((n, i) => n === arr2[i]);
+  
   return result; // boolean
 }
 
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
-
-  return resultArr; // array
+  const array = arr
+    .filter((arr) => arr > -1)
+    .filter((arr) => arr % 3 === 0)
+    .map((arr) => arr * 10)
+  
+  resultArr = array;
+  return resultArr; //array
 }
