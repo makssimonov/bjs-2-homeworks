8b3f43d3750af2a845bf4e2805accfec94c1d0a3
makssimomov <makssimonov96@gmail.com>
2022-07-13 03:10:51 +0500

    homework done

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,50 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  function Stats(name, gender, age) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+  }
+  let student1 = new Stats(name, gender, age);
+  let student2 = new Stats(name, gender, age);
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subjectName = subjectName;
+  if (this.subject === undefined) {
+    this.subject = subjectName;
+  }
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  this.mark = mark;
+  if (this.marks === undefined) {
+    this.marks = function() {
+      Stats.mark = mark;
+    }
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function (...marks) {
+  this.marks = marks;
+  let sum = 0;
+  for (let mark of marks) sum += mark;
+  return sum;
+}
+
+Student.prototype.getAverage = function() {
+    let result = this.marks.reduce(function(sum, current) {
+      return sum + current;
+    }, 0);
+    return result / this.marks.length;
+  }
+
+Student.prototype.exclude = function(reason) {
+  delete this.subject;
+  delete this.marks;
+  if (this.excluded === undefined) {
+    this.excluded = reason;
+  }
+}
